94a8e1e1ceb5b4aa149dc77865e001a26767798e
baegyeong <beth0929@naver.com>
2023-04-24 21:40:16 +0900

    feat: Print buzz
    Condition: i is multiple of 5.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -9,8 +9,12 @@ def do_fizzbuzz():
     for i in range(1, num+1):
         if i%3==0:
             print('fizz')
+        elif i%5==0:
+            print('buzz')
         else:
             print(f'{i}')
 
-    if __name__ == '__main__':
-        do_fizzbuzz(16)
+if __name__ == '__main__':
+ do_fizzbuzz(16)
+~
+
